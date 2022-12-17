5f39ef7819de0246bd719239e20580f8ec1af920
Nathan Istace <nathan.istace@gmail.com>
2022-12-17 13:25:19 +0100

    Day 14

---
+++ Day14/Day14/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Day14 {
+    internal static class Program {
+        private static void Main() {
+            Console.WriteLine($"Part 1: {Part1()}");
+            Console.WriteLine("\n-----------------\n");
+            Console.WriteLine($"Part 2: {Part2()}");
+        }
+
+
+        private enum Material {
+            Rock = 1,
+            Sand = 2
+        }
+
+        private static string Part1() {
+            var grid = ReadInput();
+            var lowerRockY = grid.Keys.Min(t => t.y);
+            var sandSpawn = new Vector2Int(500, 0);
+            var fallingForever = false;
+            while (!fallingForever) {
+                var sandPosition = sandSpawn;
+                var resting = false;
+                while (!fallingForever && !resting) {
+                    if (sandPosition.y < lowerRockY) fallingForever = true;
+                    else if (!grid.ContainsKey(sandPosition + Vector2Int.down)) sandPosition += Vector2Int.down;
+                    else if (!grid.ContainsKey(sandPosition + Vector2Int.downLeft)) sandPosition += Vector2Int.downLeft;
+                    else if (!grid.ContainsKey(sandPosition + Vector2Int.downRight)) sandPosition += Vector2Int.downRight;
+                    else {
+                        resting = true;
+                        grid.Add(sandPosition, Material.Sand);
+                    }
+                }
+            }
+
+
+            return $"{grid.Values.Count(t => t == Material.Sand)}{Environment.NewLine}{DisplayGrid(grid)}";
+        }
+
+        private static string Part2() {
+            var grid = ReadInput();
+            var lowerY = grid.Keys.Min(t => t.y);
+            var sandSpawn = new Vector2Int(500, 0);
+            while (!grid.ContainsKey(sandSpawn)) {
+                var sandPosition = sandSpawn;
+                var resting = false;
+                while (!resting) {
+                    if (sandPosition.y <= lowerY) resting = true;
+                    if (!grid.ContainsKey(sandPosition + Vector2Int.down)) sandPosition += Vector2Int.down;
+                    else if (!grid.ContainsKey(sandPosition + Vector2Int.downLeft)) sandPosition += Vector2Int.downLeft;
+                    else if (!grid.ContainsKey(sandPosition + Vector2Int.downRight)) sandPosition += Vector2Int.downRight;
+                    else resting = true;
+                }
+
+                grid.Add(sandPosition, Material.Sand);
+            }
+
+
+            return $"{grid.Values.Count(t => t == Material.Sand)}{Environment.NewLine}{DisplayGrid(grid)}";
+        }
+
+
+        private static string DisplayGrid(Dictionary<Vector2Int, Material> grid) {
+            var minX = grid.Keys.Min(t => t.x);
+            var maxX = grid.Keys.Max(t => t.x);
+            var minY = grid.Keys.Min(t => t.y);
+            const int maxY = 0;
+
+            var result = new StringBuilder();
+            for (var y = maxY; y >= minY; --y) {
+                for (var x = minX; x <= maxX; ++x) {
+                    if (!grid.ContainsKey((x, y))) result.Append('.');
+                    else if (grid[(x, y)] == Material.Rock) result.Append('#');
+                    else if (grid[(x, y)] == Material.Sand) result.Append('O');
+                }
+
+                result.Append(Environment.NewLine);
+            }
+
+            return result.ToString();
+        }
+
+
+        private static Dictionary<Vector2Int, Material> ReadInput() {
+            var result = new Dictionary<Vector2Int, Material>();
+            foreach (var rockChain in File.ReadAllLines("input.txt").Where(t => !string.IsNullOrEmpty(t)).Select(t => t.Trim().Split(" -> "))) {
+                Vector2Int? previousRockChainPoint = default;
+                foreach (var rockChainPoint in rockChain.Select(t => (Vector2Int) (int.Parse(t.Split(",")[0]), -int.Parse(t.Split(",")[1])))) {
+                    if (!result.ContainsKey(rockChainPoint)) result.Add(rockChainPoint, Material.Rock);
+                    if (previousRockChainPoint != null) {
+                        for (var step = rockChainPoint; step != previousRockChainPoint; step = step.StepTowards(previousRockChainPoint.Value)) {
+                            if (!result.ContainsKey(step)) result.Add(step, Material.Rock);
+                        }
+                    }
+
+                    previousRockChainPoint = rockChainPoint;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Day14/Day14/Vector2Int.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Day14 {
+    public struct Vector2Int {
+        public static Vector2Int up { get; } = new Vector2Int(0, 1);
+        public static Vector2Int down { get; } = new Vector2Int(0, -1);
+        public static Vector2Int right { get; } = new Vector2Int(1, 0);
+        public static Vector2Int left { get; } = new Vector2Int(-1, 0);
+        public static Vector2Int downLeft { get; } = new Vector2Int(-1, -1);
+        public static Vector2Int downRight { get; } = new Vector2Int(1, -1);
+        public int x { get; }
+        public int y { get; }
+
+        public Vector2Int(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static implicit operator Vector2Int((int x, int y) d) => new Vector2Int(d.x, d.y);
+        public static Vector2Int operator +(Vector2Int first, Vector2Int second) => new Vector2Int(first.x + second.x, first.y + second.y);
+        public static Vector2Int operator -(Vector2Int first, Vector2Int second) => new Vector2Int(first.x - second.x, first.y - second.y);
+        public static bool operator ==(Vector2Int first, Vector2Int second) => first.x == second.x && first.y == second.y;
+        public static bool operator !=(Vector2Int first, Vector2Int second) => !(first == second);
+        private bool Equals(Vector2Int other) => x == other.x && y == other.y;
+        public override bool Equals(object obj) => obj is Vector2Int other && Equals(other);
+        public override int GetHashCode() => (x * 397) ^ y;
+        public override string ToString() => $"({x:000},{y:000})";
+
+
+        /// <summary>Returns a position a step towards the given point, where this position is moved by at most one unit on the X axis and one unit on the Y axis</summary>
+        public Vector2Int StepTowards(Vector2Int other) => new Vector2Int(x + Math.Clamp(other.x - x, -1, 1), y + Math.Clamp(other.y - y, -1, 1));
+    }
+}

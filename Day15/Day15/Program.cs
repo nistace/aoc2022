497a58097024c86ef5f211b7e1ee96a89fabc9bb
Nathan Istace <nathan.istace@gmail.com>
2022-12-17 17:42:05 +0100

    Day 15

---
+++ Day15/Day15/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day15 {
+    internal static class Program {
+        private static void Main() {
+            Console.WriteLine($"Part 1: {Part1()}");
+            Console.WriteLine("\n-----------------\n");
+            Console.WriteLine($"Part 2: {Part2()}");
+        }
+
+
+        private enum CellContent {
+            Sensor = 1,
+            Beacon = 2,
+            Nothing = 3
+        }
+
+        private static string Part1() {
+            const int line = 10;
+            var lineContent = new Dictionary<int, CellContent>();
+            foreach (var (sensor, beacon, manhattanDistance) in ReadInput()) {
+                if (beacon.y == line) {
+                    if (!lineContent.ContainsKey(beacon.x)) lineContent.Add(beacon.x, CellContent.Beacon);
+                    else if (lineContent[beacon.x] == CellContent.Nothing) lineContent[beacon.x] = CellContent.Beacon;
+                }
+
+                for (var x = sensor.x - manhattanDistance; x <= sensor.x + manhattanDistance; ++x) {
+                    if (sensor.GetManhattanDistance((x, line)) <= manhattanDistance && !lineContent.ContainsKey(x))
+                        lineContent.Add(x, CellContent.Nothing);
+                }
+            }
+
+            return $"{lineContent.Count(t => t.Value == CellContent.Nothing || t.Value == CellContent.Sensor)}";
+        }
+
+        private static string Part2() {
+            const int max = 4000000;
+            var pairs = ReadInput().ToArray();
+            var candidates = pairs
+                .SelectMany(t => t.sensor.FindAllAtManhattanDistance(t.manhattanDistance + 1).Where(p => p.x >= 0 && p.x <= max && p.y >= 0 && p.y <= max))
+                .GroupBy(t => t).Where(t => t.Count() > 3)
+                .Select(t => t.Key)
+                .ToArray();
+            foreach (var candidate in candidates) {
+                if (pairs.All(u => candidate.GetManhattanDistance(u.sensor) > u.manhattanDistance)) {
+                    return $"{candidate.x * 4000000L + candidate.y}{Environment.NewLine}{candidate}";
+                }
+            }
+
+            return "ERROR, no solution.";
+        }
+
+
+        private static IEnumerable<(Vector2Int sensor, Vector2Int beacon, int manhattanDistance)> ReadInput() {
+            var result = new HashSet<(Vector2Int, Vector2Int, int)>();
+            foreach (var match in File.ReadAllLines("input.txt").Select(t => Regex.Match(t.Trim(), "Sensor.*x=([\\-\\d]+).*y=([\\-\\d]+).*x=([\\-\\d]+).*y=([\\-\\d]+)")).Where(t => t.Success)) {
+                var sensor = (Vector2Int) (int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value));
+                var beacon = (Vector2Int) (int.Parse(match.Groups[3].Value), int.Parse(match.Groups[4].Value));
+                result.Add((sensor, beacon, sensor.GetManhattanDistance(beacon)));
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Day15/Day15/Vector2Int.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day15 {
+    public readonly struct Vector2Int {
+        public static Vector2Int up { get; } = new Vector2Int(0, 1);
+        public static Vector2Int down { get; } = new Vector2Int(0, -1);
+        public static Vector2Int right { get; } = new Vector2Int(1, 0);
+        public static Vector2Int left { get; } = new Vector2Int(-1, 0);
+        public static Vector2Int downLeft { get; } = new Vector2Int(-1, -1);
+        public static Vector2Int downRight { get; } = new Vector2Int(1, -1);
+        public static Vector2Int upRight { get; } = new Vector2Int(1, 1);
+        public static Vector2Int upLeft { get; } = new Vector2Int(-1, 1);
+
+        public int x { get; }
+        public int y { get; }
+
+        public Vector2Int(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static implicit operator Vector2Int((int x, int y) d) => new Vector2Int(d.x, d.y);
+        public static Vector2Int operator +(Vector2Int first, Vector2Int second) => new Vector2Int(first.x + second.x, first.y + second.y);
+        public static Vector2Int operator -(Vector2Int first, Vector2Int second) => new Vector2Int(first.x - second.x, first.y - second.y);
+        public static bool operator ==(Vector2Int first, Vector2Int second) => first.x == second.x && first.y == second.y;
+        public static bool operator !=(Vector2Int first, Vector2Int second) => !(first == second);
+        private bool Equals(Vector2Int other) => x == other.x && y == other.y;
+        public override bool Equals(object obj) => obj is Vector2Int other && Equals(other);
+        public override int GetHashCode() => (x * 397) ^ y;
+        public override string ToString() => $"({x:000},{y:000})";
+
+        public int GetManhattanDistance(Vector2Int other) => Math.Abs(other.x - x) + Math.Abs(other.y - y);
+
+
+        /// <summary>Returns a position a step towards the given point, where this position is moved by at most one unit on the X axis and one unit on the Y axis</summary>
+        public Vector2Int StepTowards(Vector2Int other) => new Vector2Int(x + Math.Clamp(other.x - x, -1, 1), y + Math.Clamp(other.y - y, -1, 1));
+
+        public IEnumerable<Vector2Int> FindAllAtManhattanDistance(int distance) {
+            var result = new HashSet<Vector2Int>();
+            var other = new Vector2Int(x - distance, y);
+            result.Add(other);
+            foreach (var direction in new[] {upRight, downRight, downLeft, upLeft}) {
+                while ((other + direction).GetManhattanDistance(this) == distance) {
+                    other += direction;
+                    result.Add(other);
+                }
+            }
+
+            return result;
+        }
+    }
+}

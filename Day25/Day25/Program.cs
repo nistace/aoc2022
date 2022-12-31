ea3ab22df958a870afc0e24cd771806b8db4ce7d
Nathan Istace <nathan.istace@gmail.com>
2022-12-31 14:47:24 +0100

    Day 25 Part 1

---
+++ Day25/Day25/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day25 {
+    internal static class Program {
+        private static void Main() {
+            Console.WriteLine($"Part 1: {Part1()}");
+            Console.WriteLine("\n-----------------\n");
+            Console.WriteLine($"Part 2: {Part2()}");
+        }
+
+
+        private static string Part1() {
+            var input = ReadInput();
+
+            var sum = input.Aggregate(Snafu.zero, (t, u) => t + u);
+
+            return $"{sum}{Environment.NewLine}{sum}\t{sum.ToLong()}{Environment.NewLine}{string.Join(Environment.NewLine, input.Select(t => t))}";
+        }
+
+
+        private static string Part2() {
+            return "";
+        }
+
+        private static IReadOnlyList<Snafu> ReadInput() => File.ReadAllLines("input.txt").Where(t => !string.IsNullOrEmpty(t) && Snafu.TryParse(t, out _)).Select(Snafu.Parse).ToArray();
+    }
+}
---
+++ Day25/Day25/Snafu.cs
@@ -0,0 +1,54 @@
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace Day25 {
+    public struct Snafu {
+        public static Snafu zero { get; } = (Snafu) 0;
+
+        private BigInteger value { get; }
+
+
+        private Snafu(BigInteger l) => value = l;
+
+        public static implicit operator Snafu(int l) => new Snafu(l);
+        public static implicit operator Snafu(long l) => new Snafu(l);
+        public static implicit operator Snafu(BigInteger l) => new Snafu(l);
+
+        public static bool TryParse(string s, out Snafu snafu) {
+            snafu = default;
+            if (!Regex.IsMatch(s, "[012\\-=]*")) return false;
+            var value = (BigInteger) 0;
+            var coefficient = (BigInteger) 1;
+            for (var i = s.Length - 1; i >= 0; --i) {
+                value += s[i] switch {'=' => -2, '-' => -1, _ => s[i] - '0'} * coefficient;
+                coefficient *= 5;
+            }
+
+            snafu = new Snafu(value);
+            return true;
+        }
+
+        public static Snafu Parse(string s) => TryParse(s, out var snafu) ? snafu : default;
+
+
+        public override string ToString() {
+            if (value == 0) return "0";
+            if (value < 0) return "?? (<0)";
+            var tmp = value;
+            var result = "";
+            while (tmp > 0) {
+                if ((tmp % 5) == 3) result = $"={result}";
+                else if (tmp % 5 == 4) result = $"-{result}";
+                else result = $"{tmp % 5}{result}";
+                if (tmp % 5 >= 3) tmp += 5;
+                tmp /= 5;
+            }
+
+            return result;
+        }
+
+        public long ToLong() => (long) value;
+
+        public static Snafu operator +(Snafu s, Snafu t) => new Snafu(s.value + t.value);
+    }
+}
